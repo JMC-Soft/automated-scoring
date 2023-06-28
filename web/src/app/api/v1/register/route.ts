33483a64568189fa14658a0ac5d55b43242d619f
DESKTOP-OD2NKAS\sgbee <sg.been2017@gmail.com>
2023-06-28 09:51:12 +0900

    feat: register 로직 수정

--- web/src/app/api/v1/register/route.ts
+++ web/src/app/api/v1/register/route.ts
@@ -3,23 +3,29 @@ import { findUserByEmail, saveUser } from '@/app/api/repository/user';
 import { NextResponse } from 'next/server';
 
 // 신규 회원 가입
-export default async function POST(req: NextRequest) {
+export async function POST(req: NextRequest) {
   try {
     const { email, password, nickName } = await req.json();
 
     const checkUser = await findUserByEmail(email);
     if (checkUser) {
-      return NextResponse.json(new Error('이미 존재하는 회원입니다.'), {
-        status: 400,
-      });
+      return NextResponse.json(
+        { msg: '이미 존재하는 회원입니다.' },
+        {
+          status: 400,
+        },
+      );
     }
 
     const user = await saveUser(email, password, nickName);
 
     if (!user) {
-      return NextResponse.json(new Error('회원 가입에 실패했습니다.'), {
-        status: 400,
-      });
+      return NextResponse.json(
+        { msg: '회원 가입에 실패했습니다' },
+        {
+          status: 400,
+        },
+      );
     }
 
     return NextResponse.json(
@@ -32,6 +38,10 @@ export default async function POST(req: NextRequest) {
   } catch (error) {
     const message = error instanceof Error ? error.message : 'Unexpected error';
     console.log(message);
-    return NextResponse.json(new Error('서버 오류입니다.'), { status: 500 });
+    return NextResponse.json({ msg: '서버 오류입니다' }, { status: 500 });
   }
+}
+
+export async function GET() {
+  console.log('GET');
 }
