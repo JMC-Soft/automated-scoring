c35660ae45bd4ebfe91301978a60d16147b816e0
DESKTOP-OD2NKAS\sgbee <sg.been2017@gmail.com>
2023-07-03 23:34:39 +0900

    refactor: 사용중인 데이터 수 입력

--- web/src/app/api/const/dataSet.ts
+++ web/src/app/api/const/dataSet.ts
@@ -1,9 +1,9 @@
 /**
  * TODO: 데이터셋에서 각 데이터의 총 개수 정의
  */
-const HIGH_DATA_TOTAL_NUMBER = 500;
-const MIDDLE_DATA_TOTAL_NUMBER = 400;
-const LOW_DATA_TOTAL_NUMBER = 300;
+const HIGH_DATA_TOTAL_NUMBER = 713;
+const MIDDLE_DATA_TOTAL_NUMBER = null;
+const LOW_DATA_TOTAL_NUMBER = null;
 
 export {
   HIGH_DATA_TOTAL_NUMBER,
