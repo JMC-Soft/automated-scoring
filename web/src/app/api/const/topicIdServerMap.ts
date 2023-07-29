3d248e0c2238746fa9893042f559173d294d38b9
DESKTOP-OD2NKAS\sgbee <sg.been2017@gmail.com>
2023-07-30 06:40:59 +0900

    fix: server 주소 수정

--- web/src/app/api/const/topicIdServerMap.ts
+++ web/src/app/api/const/topicIdServerMap.ts
@@ -2,52 +2,52 @@ const TOPIC_ID_SERVER_MAP = {
   // 자기표현-나의 위인전
   1: {
     // 표현
-    exp0: `${process.env.FLASK_SERVER_1}/exp0`,
-    exp1: `${process.env.FLASK_SERVER_1}/exp1`,
-    exp2: `${process.env.FLASK_SERVER_1}/exp2`,
+    exp0: `${process.env.FLASK_BASE_SERVER}/biology/exp0`,
+    exp1: `${process.env.FLASK_BASE_SERVER}/biology/exp1`,
+    exp2: `${process.env.FLASK_BASE_SERVER}/biology/exp2`,
     // 구성
-    org0: `${process.env.FLASK_SERVER_1}/org0`,
-    org1: `${process.env.FLASK_SERVER_1}/org1`,
-    org2: `${process.env.FLASK_SERVER_1}/org2`,
-    org3: `${process.env.FLASK_SERVER_1}/org3`,
+    org0: `${process.env.FLASK_BASE_SERVER}/biology/org0`,
+    org1: `${process.env.FLASK_BASE_SERVER}/biology/org1`,
+    org2: `${process.env.FLASK_BASE_SERVER}/biology/org2`,
+    org3: `${process.env.FLASK_BASE_SERVER}/biology/org3`,
     // 내용
-    cont0: `${process.env.FLASK_SERVER_1}/cont0`,
-    cont1: `${process.env.FLASK_SERVER_1}/cont1`,
-    cont2: `${process.env.FLASK_SERVER_1}/cont2`,
+    cont0: `${process.env.FLASK_BASE_SERVER}/biology/cont0`,
+    cont1: `${process.env.FLASK_BASE_SERVER}/biology/cont1`,
+    cont2: `${process.env.FLASK_BASE_SERVER}/biology/cont3`,
   },
 
-  // 설득-혐오 시설에 대한 본인의 생각
+  //
   2: {
     // 표현
-    exp0: `${process.env.FLASK_SERVER_2}/exp0`,
-    exp1: `${process.env.FLASK_SERVER_2}/exp1`,
-    exp2: `${process.env.FLASK_SERVER_2}/exp2`,
+    exp0: `${process.env.FLASK_BASE_SERVER}/character/exp0`,
+    exp1: `${process.env.FLASK_BASE_SERVER}/character/exp1`,
+    exp2: `${process.env.FLASK_BASE_SERVER}/character/exp2`,
     // 구성
-    org0: `${process.env.FLASK_SERVER_2}/org0`,
-    org1: `${process.env.FLASK_SERVER_2}/org1`,
-    org2: `${process.env.FLASK_SERVER_2}/org2`,
-    org3: `${process.env.FLASK_SERVER_2}/org3`,
+    org0: `${process.env.FLASK_BASE_SERVER}/character/org0`,
+    org1: `${process.env.FLASK_BASE_SERVER}/character/org1`,
+    org2: `${process.env.FLASK_BASE_SERVER}/character/org2`,
+    org3: `${process.env.FLASK_BASE_SERVER}/character/org3`,
     // 내용
-    cont0: `${process.env.FLASK_SERVER_2}/cont0`,
-    cont1: `${process.env.FLASK_SERVER_2}/cont1`,
-    cont2: `${process.env.FLASK_SERVER_2}/cont2`,
+    cont0: `${process.env.FLASK_BASE_SERVER}/character/cont0`,
+    cont1: `${process.env.FLASK_BASE_SERVER}/character/cont1`,
+    cont2: `${process.env.FLASK_BASE_SERVER}/character/cont3`,
   },
 
   // 정보 전달-영화_독서 감상문
   3: {
     // 표현
-    exp0: `${process.env.FLASK_SERVER_3}/exp0`,
-    exp1: `${process.env.FLASK_SERVER_3}/exp1`,
-    exp2: `${process.env.FLASK_SERVER_3}/exp2`,
+    exp0: `${process.env.FLASK_BASE_SERVER}/review/exp0`,
+    exp1: `${process.env.FLASK_BASE_SERVER}/review/exp1`,
+    exp2: `${process.env.FLASK_BASE_SERVER}/review/exp2`,
     // 구성
-    org0: `${process.env.FLASK_SERVER_3}/org0`,
-    org1: `${process.env.FLASK_SERVER_3}/org1`,
-    org2: `${process.env.FLASK_SERVER_3}/org2`,
-    org3: `${process.env.FLASK_SERVER_3}/org3`,
+    org0: `${process.env.FLASK_BASE_SERVER}/review/org0`,
+    org1: `${process.env.FLASK_BASE_SERVER}/review/org1`,
+    org2: `${process.env.FLASK_BASE_SERVER}/review/org2`,
+    org3: `${process.env.FLASK_BASE_SERVER}/review/org3`,
     // 내용
-    cont0: `${process.env.FLASK_SERVER_3}/cont0`,
-    cont1: `${process.env.FLASK_SERVER_3}/cont1`,
-    cont2: `${process.env.FLASK_SERVER_3}/cont2`,
+    cont0: `${process.env.FLASK_BASE_SERVER}/review/cont0`,
+    cont1: `${process.env.FLASK_BASE_SERVER}/review/cont1`,
+    cont2: `${process.env.FLASK_BASE_SERVER}/review/cont3`,
   },
 };
 
